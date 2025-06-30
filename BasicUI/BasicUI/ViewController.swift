072e8e7f44efec24338ea407815d113fcf554d48
M Matango <lmatango@ec.krugercorp.com>
2025-06-30 11:59:37 -0500

    add new seccion

---
+++ BasicUI/BasicUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicUI
+//
+//  Created by Mauricio Matango on 30/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BasicUI/BasicUITests/BasicUITests.swift
@@ -0,0 +1,17 @@
+//
+//  BasicUITests.swift
+//  BasicUITests
+//
+//  Created by Mauricio Matango on 30/6/25.
+//
+
+import Testing
+@testable import BasicUI
+
+struct BasicUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
