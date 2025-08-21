734bebfd4043828f43b028e079a6003b70aa1170
M Matango <lmatango@ec.krugercorp.com>
2025-08-21 16:46:47 -0500

    add first app

---
+++ Hello Xcode/Hello Xcode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hello Xcode
+//
+//  Created by Mauricio Matango on 21/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hello Xcode/Hello Xcode/Hello_XcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_XcodeApp.swift
+//  Hello Xcode
+//
+//  Created by Mauricio Matango on 21/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Hello_XcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
