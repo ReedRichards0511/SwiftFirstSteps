9200cfb915cdfe2fc847374aa2407a1efff7753a
M Matango <lmatango@ec.krugercorp.com>
2025-08-25 17:14:44 -0500

    add inages

--- Hello Xcode/Hello Xcode/ContentView.swift
+++ Hello Xcode/Hello Xcode/ContentView.swift
@@ -12,21 +12,43 @@ struct ContentView: View {
         ZStack {
             //backrhoun color
             LinearGradient(
-                colors:[Color.white, Color.green, Color.blue.opacity(0.4)] ,
+                colors:[Color.blue, Color.white, Color.indigo.opacity(0.4)],
                 startPoint: .top,
                 endPoint: .bottomTrailing
             )
                 .ignoresSafeArea()//full screen color applied
             
             VStack {
-                Image(systemName: "person.fill")
+                Image(systemName: "figure.skiing.downhill")
                     .imageScale(.large)
-                    .foregroundStyle(.white)
-                Text("Hello, Xcode!")
+                    
+                
+                Text("Welcome to iOS development !")
                     .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                
+                Text("First App")
+                    .font(.title)
                     .fontWeight(.thin)
+                    .padding()
+                    .background(Color.blue.opacity(0.8))
+                    
+             
+                Text("I love to code")
+                Text("Please  feel free to ask questions, send requests via the course Q&a or direct message me")
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .foregroundStyle(.red)
+                    .font(.title2)
+                Image(.apple)
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                 
             }
             .padding()
+           
         }
     }
 }
